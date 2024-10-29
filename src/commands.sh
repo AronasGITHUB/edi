bf087314ae0342364d8d118ccc15cd45b8945c3a
AronazGithubz <a.finocenka.lubuntu@gmail.com>
2024-10-29 15:03:49 +0100

    Update commands.sh

--- src/commands.sh
+++ src/commands.sh
@@ -4,6 +4,8 @@
 # This is a file of commands in edi.
 # Check our license to see what can you do with this script.
 
+# THIS FILE IS NOT MEANT TO BE EXECUTED, THESE ARE SCRIPTS YOU CAN TAKE.
+
 # In case statements, and ";;" at the end.
 
 # Like, this example:
@@ -14,3 +16,7 @@
 # h
 # Please provide a seperate function named 'hlp'
 hlp
+
+# q
+clear
+exit 0
